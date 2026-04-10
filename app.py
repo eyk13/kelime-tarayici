0bccf7722040ef78f8aafe21b76d59f13ef312d2
Eda Yurdagül Deniz <edadeniz@Eda-MacBook-Air.local>
2026-04-10 17:50:09 +0300

    port duzeltildi

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def ara():
     return render_template("index.html", arama_sonuc=sayi, aranan=kelime)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
